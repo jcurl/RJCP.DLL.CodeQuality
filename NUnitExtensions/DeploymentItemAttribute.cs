407ee713028b4f33301f411ac2b026c5b6bd8a74
Jason Curl <jason.curl@harman.com>
2016-08-09 20:08:45 +0200

    Rework DeploymentItemAttribute to handle parallel copying by retrying.
    On slow disks we have a much higher chance of running into an exception of trying to copy a file that is already in the process of being copied. That occurs because after what's on disk is not what we would expect, two methods then start copying. Typical errors are listed in code. We don't want a global lock, so we catch the exceptions that occur, sleep 250ms and try again. We might try again if the file exists. Hopefully the copy is finished within 1 second and all is good. If it isn't, we ignore it and continue. Why do we ignore it? Either the user had an error and the results are undefined anyway (see the summary remarks), or it's already copied.
    
    Further change includes trimming the output directory. If it contains spaces at the end or beginning, we remove them now.
    
    Tested by using the console version of nUnit which makes exceptions really obvious (it still doesn't print to the console though). Tested on a really slow disk that showed the problems, and on a fast SSD that never showed any problems.
    
    Issue: MGUSIN-7984

--- NUnitExtensions/DeploymentItemAttribute.cs
+++ NUnitExtensions/DeploymentItemAttribute.cs
@@ -9,8 +9,24 @@ namespace NUnit.Framework
     /// Class DeploymentItemAttribute.
     /// </summary>
     /// <remarks>
-    /// Takes a compatible MSTest type DeploymentItem attribute for usage with nUnit. This code was taken from
-    /// http://stackoverflow.com/questions/9378276/nunit-deploymentitem.
+    /// Takes a compatible MSTest type DeploymentItem attribute for usage with nUnit. You specify this for the test
+    /// case, or the test fixture. As NUnit reflects over the assembly looking for test csaes, it will reflect over
+    /// this attribute also causing the copy to occur. Therefore, the deployment occurs during test case discovery
+    /// regardless if your test case is being executed or not.
+    /// <para>This attribute will work for nUnit shadow copy enabled or disabled, as it relies on the test case
+    /// assembly <see cref="Assembly.CodeBase"/> property.</para>
+    /// <para>When using this attribute, ensure that two test cases don't clobber each other. This might occur
+    /// if you try to do strange things such as deploying directory A/B in one test case and then deploying the
+    /// parent directory A in a different test case.</para>
+    /// <para>Before the file is deployed, it is checked if the copy has already been done by comparing the length,
+    /// the create timestamp and modify timestamp. If any of these differ, the file is copied. If they're all the same,
+    /// no copy occurs. This makes the copy as fast as possible and ensures also that files can't be copied on top
+    /// of themselves.</para>
+    /// <para>When deploying directories, the files are <b>merged</b> with the destination directory.</para>
+    /// <para>If you have two different files that are being deployed to the same location as the same name, the
+    /// results are undefined. Don't do it. Your test cases may pass or fail depending on the position of the moon,
+    /// or if your cat just sneezed a few minutes ago. The same applies if your deploying two different directories
+    /// to the same location that have different content but where filenames overlap.</para>
     /// </remarks>
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = true, Inherited = false)]
     public class DeploymentItemAttribute : Attribute
@@ -20,7 +36,7 @@ namespace NUnit.Framework
         /// Marks an item to be relevant for a unit-test and copies it to deployment-directory for this unit-test.
         /// </summary>
         /// <param name="path">The relative or absolute path to the file or directory to deploy. The path is relative to the build output directory.</param>
-        public DeploymentItemAttribute(string path) : this (path, null) { }
+        public DeploymentItemAttribute(string path) : this(path, null) { }
 
         /// <summary>
         /// NUnit replacement for Microsoft.VisualStudio.TestTools.UnitTesting.DeploymentItemAttribute
@@ -42,69 +58,103 @@ namespace NUnit.Framework
             // Get the target-path where to copy the deployment item to
             string binFolderPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
 
+            // Trim any spaces. It's not a good idea if the user provided spaces in the directory name before or after.
+            // Most OSes and/or people get confused otherwise.
+            string normalizedDir = outputDirectory == null ? string.Empty : outputDirectory.Trim();
             string itemPathInBin;
-            if (string.IsNullOrEmpty(outputDirectory)) {
+            if (string.IsNullOrEmpty(normalizedDir)) {
                 itemPathInBin = new Uri(Path.Combine(binFolderPath, itemName)).LocalPath;
-            } else if (!string.IsNullOrEmpty(Path.GetPathRoot(outputDirectory))) {
-                itemPathInBin = new Uri(Path.Combine(outputDirectory, itemName)).LocalPath;
+            } else if (!string.IsNullOrEmpty(Path.GetPathRoot(normalizedDir))) {
+                itemPathInBin = new Uri(Path.Combine(normalizedDir, itemName)).LocalPath;
             } else {
-                itemPathInBin = new Uri(Path.Combine(binFolderPath, outputDirectory, itemName)).LocalPath;
+                itemPathInBin = new Uri(Path.Combine(binFolderPath, normalizedDir, itemName)).LocalPath;
             }
 
-            try {
-                if (File.Exists(itemPath)) {
-                    string parentFolderPathInBin = new DirectoryInfo(itemPathInBin).Parent.FullName;
+            if (File.Exists(itemPath)) {
+                string parentFolderPathInBin = new DirectoryInfo(itemPathInBin).Parent.FullName;
 
-                    if (!File.Exists(itemPathInBin)) {
-                        if (!Directory.Exists(parentFolderPathInBin)) {
-                            Directory.CreateDirectory(parentFolderPathInBin);
-                        }
+                if (!File.Exists(itemPathInBin)) {
+                    if (!CreateDirectory(parentFolderPathInBin)) {
+                        Console.WriteLine("Could not create directory at {0}", itemPathInBin);
                     }
+                }
 
-                    if (!CopyFile(itemPath, itemPathInBin)) {
+                if (!CopyFile(itemPath, itemPathInBin)) {
+                    Console.WriteLine("Could not copy from {0} to {1}", itemPath, itemPathInBin);
+                }
+            } else if (Directory.Exists(itemPath)) {
+                if (!CreateDirectory(itemPathInBin)) {
+                    Console.WriteLine("Could not create directory at {0}", itemPathInBin);
+                }
+                foreach (string dirPath in Directory.GetDirectories(itemPath, "*", SearchOption.AllDirectories)) {
+                    string destinationDir = dirPath.Replace(itemPath, itemPathInBin);
+                    if (!CreateDirectory(destinationDir)) {
+                        Console.WriteLine("Could not create directory at {0}", destinationDir);
+                    }
+                }
+
+                foreach (string sourcePath in Directory.GetFiles(itemPath, "*.*", SearchOption.AllDirectories)) {
+                    string destinationPath = sourcePath.Replace(itemPath, itemPathInBin);
+                    if (!CopyFile(sourcePath, destinationPath)) {
                         Console.WriteLine("Could not copy from {0} to {1}", itemPath, itemPathInBin);
                     }
-                } else if (Directory.Exists(itemPath)) {
-                    if (Directory.Exists(itemPathInBin)) {
-                        Directory.Delete(itemPathInBin, true);
-                    }
-
-                    Directory.CreateDirectory(itemPathInBin);
-                    foreach (string dirPath in Directory.GetDirectories(itemPath, "*", SearchOption.AllDirectories)) {
-                        Directory.CreateDirectory(dirPath.Replace(itemPath, itemPathInBin));
-                    }
-
-                    foreach (string sourcePath in Directory.GetFiles(itemPath, "*.*", SearchOption.AllDirectories)) {
-                        string destinationPath = sourcePath.Replace(itemPath, itemPathInBin);
-                        if (!CopyFile(sourcePath, destinationPath)) {
-                            Console.WriteLine("Could not copy from {0} to {1}", itemPath, itemPathInBin);
-                        }
-                    }
-                } else {
-                    Console.WriteLine("Warning: Deployment item does not exist - \"" + itemPath + "\"");
                 }
-            } catch (System.Exception ex) {
-                Console.WriteLine("Exception during deployment: {0}", ex.ToString());
-                throw;
             }
+        }
+
+        private static bool CreateDirectory(string directory)
+        {
+            if (Directory.Exists(directory)) return true;
+
+            // Creates all directories and subdirectories in the specified path. So we don't need to create each
+            // individual directory.
+            try {
+                Directory.CreateDirectory(directory);
+            } catch (UnauthorizedAccessException) {
+                // Permissions problem, couldn't create the directory...
+                return false;
+            }
+            return true;
         }
 
         private static bool CopyFile(string source, string destination)
         {
             if (!File.Exists(source)) return false;
 
-            FileInfo itemInfo = new FileInfo(source);
+            bool copyFinished = false;
+            int attempts = 4;  // Retry every 250ms meaning maximum timeout of 1s.
+            FileInfo itemInfo;
+            do {
+                itemInfo = new FileInfo(source);
 
-            // Check if we need to copy the file. We only do so if it doesn't exist or if it's
-            // different (regardless of why).
-            if (File.Exists(destination)) {
-                FileInfo itemPathInBinInfo = new FileInfo(destination);
-                if (itemInfo.Length == itemPathInBinInfo.Length &&
-                    itemInfo.LastWriteTime == itemPathInBinInfo.LastWriteTime &&
-                    itemInfo.CreationTime == itemPathInBinInfo.CreationTime) return true;
-            }
+                // Check if we need to copy the file. We only do so if it doesn't exist or if it's
+                // different (regardless of why).
+                if (File.Exists(destination)) {
+                    FileInfo itemPathInBinInfo = new FileInfo(destination);
+                    if (itemInfo.Length == itemPathInBinInfo.Length &&
+                        itemInfo.LastWriteTime == itemPathInBinInfo.LastWriteTime &&
+                        itemInfo.CreationTime == itemPathInBinInfo.CreationTime) return true;
+                }
 
-            File.Copy(source, destination, true);
+                bool fail = false;
+                try {
+                    File.Copy(source, destination, true);
+                    copyFinished = true;
+                } catch (AccessViolationException) {
+                    fail = true;
+                } catch (UnauthorizedAccessException) {
+                    // On windows occurs if the file is already open.
+                    fail = true;
+                }
+                if (fail) {
+                    // If the copy failed, it's because it's probably already open and being copied already
+                    // from another instance of DeployItemAttribute. So we wait 250ms and try again. The race
+                    // condition occurs because at the time of the check it didn't exist, but between that
+                    // and now the copy has started elsewhere.
+                    --attempts;
+                    if (attempts > 0) System.Threading.Thread.Sleep(250);
+                }
+            } while (!copyFinished && attempts > 0);
 
             // Allow destination file to be deletable and set the creation time to be identical to the source
             FileAttributes fileAttributes = File.GetAttributes(destination);
