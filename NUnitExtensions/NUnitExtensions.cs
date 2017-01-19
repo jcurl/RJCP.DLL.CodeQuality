b697305ae5642dcb65f2939dbae7180ce6d10d54
Jason Curl <jason.curl@harman.com>
2017-01-19 13:12:24 +0100

    Remove references in documentation to NUnit.
    This causes unresolved references during the build of XML documentation with SHFB. Adding nunit.framework.xml to the project adds a significant number of other references that are outside of the scope of this project.
    
    Issue: HELIOS-21

--- NUnitExtensions/NUnitExtensions.cs
+++ NUnitExtensions/NUnitExtensions.cs
@@ -9,9 +9,9 @@
     /// You can derive from the base class <see cref="NUnitExtensions"/> to gain extra functionality in
     /// addition to the standard functionality from NUnit 2.6.4.
     /// <para><b>Automation Deployment of Files:</b></para>
-    /// <para>When you derive from this class, it implements a <see cref="NUnit.Framework.TestFixtureSetUpAttribute"/>
+    /// <para>When you derive from this class, it implements a <b>TestFixtureSetUpAttribute</b>
     /// that will automatically deploy files that are decorated with the <see cref="DeploymentItemAttribute"/>. You
-    /// should still decorate your classes with the <see cref="NUnit.Framework.TestFixtureAttribute"/>.</para>
+    /// should still decorate your classes with the <b>TestFixtureAttribute</b>.</para>
     /// <code language="csharp">
     /// [TestFixture(Category = "NUnitExtensions.Deployment.BaseClass")]
     /// public class NUnitExtensionsBaseClassTest : NUnitExtensions
@@ -34,7 +34,7 @@
         /// When implementing your own test case, you will want to override this method and then call the base
         /// if you have extra work to do during the test case fixture set up.
         /// <para>If an exception occurs in this method (during the deployment), NUnit will likely complain
-        /// about an exception in the <see cref="TestFixtureSetUpAttribute"/>. Please look at the console output
+        /// about an exception in the <b>TestFixtureSetUpAttribute</b>. Please look at the console output
         /// to identify the error.</para>
         /// </remarks>
         [TestFixtureSetUp]
