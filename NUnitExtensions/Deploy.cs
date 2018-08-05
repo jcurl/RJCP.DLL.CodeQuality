d4cad00fec814777fe405b19fa75f2b08c213faa
Jason Curl <jason.curl@harman.com>
2018-08-05 10:36:34 +0200

    NUnitExtensions: Upgrade to NUnit 2.6.7 and remove DeploymentItemAttribute
    When moving to NUnit 2.6.7, the [TestFixtureSetUp] attribute should be replaced with [OneTimeSetUp]. Unfortunately, it doesn't work and if we move to NUnit 3.x (which is the reason to moving to NUnit 2.6.7 earlier) there is no solution. Given that the [OneTimeSetUp] doesn't actually get executed, remove the entire concept of the base class.
    
    By removing the base class, test cases will need to explicitly deploy files with Deploy.Item(). This is arguably better, as it is already observed some test cases use the [DeploymentItem] attribute, but forget to derive from NUnitExtensions (which is an error, and would result in no deployment).
    
    This project no longer has a dependency to NUnit since the removal of the base class NUnitExtensions and so has been removed.
    
    Issue: HELIOS-548

--- NUnitExtensions/Deploy.cs
+++ NUnitExtensions/Deploy.cs
@@ -13,10 +13,8 @@
     /// <remarks>
     /// These methods are used to control when files should be deployed along side a test case,
     /// as NUnit doesn't have any mechanism to do this, unlike the Microsoft Test Framework.
-    /// <para>You can decorate the test methods with the <seealso cref="DeploymentItemAttribute"/>,
-    /// or specifically request deployment with <seealso cref="Deploy.Item(string, string)"/>
-    /// within your test case. When using the attribute, you still need to add code to your test
-    /// case to request deployment with <seealso cref="Deploy.ItemsWithAttribute(object)"/>.</para>
+    /// <para>You can specifically request deployment with <seealso cref="Deploy.Item(string, string)"/>
+    /// within your test case.</para>
     /// </remarks>
     public static class Deploy
     {
@@ -25,137 +23,6 @@
         private const int DeleteWaitInterval = 250;
         private const int CopyWaitInterval = 250;
         private const int CopyWaitAttempts = 4;
-
-        /// <summary>
-        /// Deploy files for all methods containing the attribute <see cref="DeploymentItemAttribute"/> for the given class.
-        /// </summary>
-        /// <param name="testClass">The test class to deploy files for.</param>
-        /// <exception cref="ArgumentNullException"><paramref name="testClass"/> may not be <see langword="null"/>.</exception>
-        /// <exception cref="ArgumentException">Error when deploying test artifacts. The errors are printed to the
-        /// console.</exception>
-        /// <remarks>
-        /// You need to ensure that the methods for the class you're referencing are decorated with the
-        /// <see cref="DeploymentItemAttribute"/>. See the example below.
-        ///
-        /// <code language="csharp">
-        /// [TestFixture]
-        /// public class NUnitExtensionsTest {
-        ///     [TestFixtureSetUp]
-        ///     public void TestFixtureSetUp() {
-        ///         Deploy.ItemsWithAttribute(this);
-        ///     }
-        ///
-        ///     [Test]
-        ///     [DeploymentItem("test.txt")]
-        ///     public void MyTest() {
-        ///         Assert.That(File.Exists("test.txt"));
-        ///     }
-        /// }
-        /// </code>
-        /// <para>During deployment, as many files are deployed as possible. However, if there are errors when
-        /// deploying all the files, the exception <see cref="ArgumentException"/> will be raised.</para>
-        /// </remarks>
-        public static void ItemsWithAttribute(object testClass)
-        {
-            if (testClass == null) throw new ArgumentNullException(nameof(testClass));
-            ItemsWithAttribute(testClass.GetType());
-        }
-
-        /// <summary>
-        /// Deploy files for all methods containing the attribute <see cref="DeploymentItemAttribute"/> for the given class.
-        /// </summary>
-        /// <param name="testClass">The test class to deploy files for.</param>
-        /// <exception cref="ArgumentNullException"><paramref name="testClass"/> may not be <see langword="null"/></exception>
-        /// <exception cref="ArgumentException">Error when deploying test artifacts. The errors are printed to the standard
-        /// console.</exception>
-        /// <remarks>
-        /// You need to ensure that the methods for the class you're referencing are decorated with the
-        /// <see cref="DeploymentItemAttribute"/>. See the example below.
-        ///
-        /// <code language="csharp">
-        /// [TestFixture]
-        /// public class NUnitExtensionsTest {
-        ///     [TestFixtureSetUp]
-        ///     public void TestFixtureSetUp() {
-        ///         Deploy.ItemsWithAttribute(typeof(this));
-        ///     }
-        ///
-        ///     [Test]
-        ///     [DeploymentItem("test.txt")]
-        ///     public void MyTest() {
-        ///         Assert.That(File.Exists("test.txt"));
-        ///     }
-        /// }
-        /// </code>
-        /// <para>During deployment, as many files are deployed as possible. However, if there are errors when
-        /// deploying all the files, the exception <see cref="ArgumentException"/> will be raised.</para>
-        /// </remarks>
-        public static void ItemsWithAttribute(Type testClass)
-        {
-            bool exceptionFound = false;
-            if (testClass == null) throw new ArgumentNullException(nameof(testClass));
-            foreach (MethodInfo method in testClass.GetMethods(BindingFlags.Public | BindingFlags.Instance)) {
-                exceptionFound |= DeployMember(method);
-            }
-
-            if (exceptionFound) {
-                throw new ArgumentException("Error deploying test artifacts", nameof(testClass));
-            }
-        }
-
-        private static bool DeployMember(MethodInfo method)
-        {
-            IEnumerable<DeploymentItemAttribute> attributes = GetAttribute<DeploymentItemAttribute>(method);
-            if (attributes == null) return false;
-
-            bool exceptionFound = false;
-            foreach (DeploymentItemAttribute attribute in attributes) {
-                try {
-                    Item(attribute.Path, attribute.OutputDirectory);
-                } catch (Exception e) {
-                    if (!ReportException(e, method.DeclaringType.Name, method.Name, attribute.Path, attribute.OutputDirectory)) {
-                        throw;
-                    } else {
-                        exceptionFound = true;
-                    }
-                }
-            }
-            return exceptionFound;
-        }
-
-        private static bool ReportException(Exception e, string className, string methodName, string sourcePath, string destDir)
-        {
-            Console.WriteLine("Exception deploying class: {0}; method: {1}", className, methodName);
-            Console.WriteLine("  Copying from '{0}' to '{1}'", sourcePath, destDir);
-
-            if (e is FileNotFoundException) {
-                Console.WriteLine("File not found in test case deployment: {0}", e.Message);
-            } else if (e is DirectoryNotFoundException) {
-                Console.WriteLine("Directory not found in test case deployment: {0}", e.Message);
-            } else if (e is UnauthorizedAccessException) {
-                Console.WriteLine("Access denied in test case deployment: {0}", e.Message);
-            } else if (e is PathTooLongException) {
-                Console.WriteLine("Path too long in test case deployment: {0}", e.Message);
-            } else if (e is NotSupportedException) {
-                Console.WriteLine("Invalid characters in path in test case deployment: {0}", e.Message);
-            } else if (e is ArgumentNullException) {
-                Console.WriteLine("Path is null in test case deployment: {0}", e.Message);
-            } else if (e is ArgumentException) {
-                Console.WriteLine("Argument exception in test case deployment: {0}", e.Message);
-            } else if (e is IOException) {
-                Console.WriteLine("I/O Exception in test case deployment: {0}", e.Message);
-            } else {
-                Console.WriteLine("Unhandled Exception: {0}", e.ToString());
-                return false;
-            }
-
-            return true;
-        }
-
-        private static IEnumerable<T> GetAttribute<T>(ICustomAttributeProvider provider) where T : Attribute
-        {
-            return provider.GetCustomAttributes(typeof(T), false).OfType<T>().ToArray();
-        }
 
         /// <summary>
         /// In line test case method for deployment of a test resource.
@@ -173,7 +40,7 @@
         /// <exception cref="ArgumentNullException"><paramref name="path"/> may not be <see langword="null"/>.</exception>
         /// <remarks>
         /// This method can deploy files within your test case at a time of your choosing.
-        /// <para>This attribute will work for nUnit shadow copy enabled or disabled, as it relies on the test case
+        /// <para>This method will work for nUnit shadow copy enabled or disabled, as it relies on the test case
         /// assembly <see cref="System.Reflection.Assembly.CodeBase"/> property.</para>
         /// <para>If you have two different files that are being deployed to the same location with the same name, the
         /// results are undefined. Don't do it. Your test cases may pass or fail depending on the position of the moon,
@@ -204,11 +71,11 @@
         /// <exception cref="ArgumentNullException"><paramref name="path"/> may not be <see langword="null"/>.</exception>
         /// <remarks>
         /// This method can deploy files within your test case at a time of your choosing.
-        /// <para>This attribute will work for nUnit shadow copy enabled or disabled, as it relies on the test case
+        /// <para>This method will work for nUnit shadow copy enabled or disabled, as it relies on the test case
         /// assembly <see cref="System.Reflection.Assembly.CodeBase"/> property.</para>
         /// <para>When files and folders are copied to the output directory, they are <i>merged</i> with the content
         /// of the folder that may already exist.</para>
-        /// <para>When using this attribute, ensure that two test cases don't clobber each other. This might occur
+        /// <para>When using this method, ensure that two test cases don't clobber each other. This might occur
         /// if you try to do strange things such as deploying directory A/B in one test case and then deploying the
         /// parent directory A in a different test case.</para>
         /// <para>Before the file is deployed, it is checked if the copy has already been done by comparing the length,
@@ -447,7 +314,7 @@
 
                 if (!copyFinished) {
                     // If the copy failed, it's because it's probably already open and being copied already
-                    // from another instance of DeployItemAttribute. So we wait 250ms and try again. The race
+                    // from another instance of Deploy. So we wait 250ms and try again. The race
                     // condition occurs because at the time of the check it didn't exist, but between that
                     // and now the copy has started elsewhere.
                     --attempts;
--- NUnitExtensionsTest/DeployForwardPathTest.cs
+++ NUnitExtensionsTest/DeployForwardPathTest.cs
@@ -6,50 +6,6 @@
     [TestFixture(Category = "NUnitExtensions.Deployment.ForwardPath")]
     public class DeployForwardPathTest
     {
-        [TestFixtureSetUp]
-        public void TestFixtureSetUp()
-        {
-            Deploy.ItemsWithAttribute(this);
-        }
-
-        [Test]
-        [DeploymentItem("Resources/test1.txt")]
-        public void DeployFile()
-        {
-            Assert.That(File.Exists("test1.txt"));
-        }
-
-        [Test]
-        [DeploymentItem("Resources", "folder")]
-        public void DeployFolder()
-        {
-            Assert.That(File.Exists(Path.Combine("folder", "Resources", "test1.txt")), "File 'folder/Resources/test1.txt' not found");
-            Assert.That(File.Exists(Path.Combine("folder", "Resources", "test2.txt")), "File 'folder/Resources/test2.txt' not found");
-        }
-
-        [Test]
-        [DeploymentItem("Resources/test1.txt", "Resources")]
-        public void DeployToSource()
-        {
-            Assert.That(File.Exists(Path.Combine("Resources", "test1.txt")), "File 'folder/Resources/test1.txt' not found");
-        }
-
-        [Test]
-        [DeploymentItem("Resources/test1.txt", "Resources")]
-        public void DeployToSourceSecondCopy()
-        {
-            Assert.That(File.Exists(Path.Combine("Resources", "test1.txt")), "File 'folder/Resources/test1.txt' not found");
-        }
-
-        [Test]
-        [DeploymentItem("Resources/test1.txt", "files")]
-        [DeploymentItem("Resources/test2.txt", "files")]
-        public void DeployTwoItemsToOtherDirectory()
-        {
-            Assert.That(File.Exists(Path.Combine("files", "test1.txt")), "File 'files/test1.txt' not found");
-            Assert.That(File.Exists(Path.Combine("files", "test2.txt")), "File 'files/test2.txt' not found");
-        }
-
         [Test]
         [Repeat(100)]
         public void DeployFileInLine()
