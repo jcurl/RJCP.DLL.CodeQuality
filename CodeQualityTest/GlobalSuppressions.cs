ad0f45ef052114ebf300086bcfa5be6827809d15
Jason Curl <jcurl@arcor.de>
2024-03-03 12:25:20 +0100

    CodeQuality: Suppress IDE0230 for UTF-8 string literals
    The test cases use bytes, that just so happens to be the same as a string literal. But the test case should use the bytes, as that is the underlying interface.
    
    Issue: DOTNET-995

--- CodeQualityTest/GlobalSuppressions.cs
+++ CodeQualityTest/GlobalSuppressions.cs
@@ -8,6 +8,7 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Style", "IDE0056:Use index operator", Justification = ".NET Core only feature")]
 [assembly: SuppressMessage("Style", "IDE0057:Use range operator", Justification = ".NET Core only feature")]
 [assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Test Methods used for reflection")]
+[assembly: SuppressMessage("Style", "IDE0230:Use UTF-8 string literal", Justification = "Testing byte streams, not UTF8 strings")]
 [assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Test Methods used for reflection")]
 [assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations", Justification = ".NET Core only feature")]
 [assembly: SuppressMessage("Performance", "CA1861:Avoid constant arrays as arguments", Justification = "Test Case Only")]
